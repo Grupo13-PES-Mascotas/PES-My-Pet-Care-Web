1d6ab306c4b1a3bf68ca9ec898a191e2786ded1a
enrichp <enrichernandopuerta@gmail.com>
2020-06-04 11:05:24 +0200

    Google firebase login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatGridListModule} from "@angular/material/grid-list";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
 import {AppRoutingModule} from './app-routing.module';
 import {LogInComponent} from './log-in/log-in.component';
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatListModule} from "@angular/material/list";
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,6 +1,7 @@
 import {Pet} from './pet';
 
 export interface User {
+  uid: string;
   username: string;
   email: string;
   pets?: Pet[];
