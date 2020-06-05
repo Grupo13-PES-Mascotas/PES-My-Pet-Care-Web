abcfcb23168e299e8e7f0688eaf3e1db4fa17aef
enrichp <enrichernandopuerta@gmail.com>
2020-06-05 21:52:14 +0200

    Images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,23 +18,6 @@ import {DialogPetComponent} from './dialog/dialog.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'My Pet Care';
-  user: User;
-  isLogin: boolean = true;
-  selectedNavigation: number = 0;
-  selectedCustomOptionNavigation: number = 0;
-  arePetsObtained: boolean = false;
-  registerPetForm;
-  img: string = '';
-  navigationOptions = [
-    'My Pets', 'Pet\'s Community', 'My Walks', 'Near Establishments', 'Calendar', 'Achievements', 'Settings'
-  ];
-  navigationCustomOptions = [
-    ['Register new pet'], [], [], [], [], [], []
-  ];
-  registerPetControl = new FormControl('', [
-    Validators.required
-  ]);
 
   constructor(
     private router: Router,
@@ -47,103 +30,23 @@ export class AppComponent implements OnInit {
     public dialog: MatDialog,
   ) {
   }
-
-  doGoogleLogin() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    provider.addScope('profile');
-    provider.addScope('email');
-    this.afAuth.signInWithPopup(provider).then(res => {
-      this.tokenManagerService.token = res.user.refreshToken;
-      this.userApiService.getUserOld(res.user.uid).subscribe(user => {
-        this.user = user;
-        this.petApiService.getAllPetsOld(res.user.displayName).subscribe(pets => {
-          this.user.pets = pets;
-          this.isLogin = false;
-          this.arePetsObtained = true;
-          for (const pet of pets ) {
-            this.petApiService.getPetImage(res.user.displayName, pet.name).subscribe(img => {
-              pet.image = img;
-            });
-          };
-        });
-      });
-    });
-  }
-
-  doLogOut() {
-    this.afAuth.signOut().then(() => {
-      this.user = null;
-      this.isLogin = true;
-      this.arePetsObtained = false;
-    });
-  }
-
-  info(pet: Pet) {
-    pet.owner = this.user.username;
-    const dialog = this.dialog.open(DialogPetComponent, {
-      data: pet
-    });
-    dialog.afterClosed().subscribe(() => {
-      this.petApiService.getAllPetsOld(this.user.username).subscribe(pets => {
-        this.user.pets = pets;
-        this.isLogin = false;
-        this.arePetsObtained = true;
-      });
-    });
-  }
-
-  ngOnInit(): void {
-    this.registerPetForm = this.formBuilder.group({
-      name: '',
-      gender: '',
-      breed: '',
-      birth: '',
-      pathologies: ''
-    });
-
-  }
-
-  getDate(dateTime: string): string {
-    let index = dateTime.indexOf('T');
-    let date = dateTime.substring(0, index);
-    let dateParts = date.split('-');
-
-    return dateParts[2] + '-' + dateParts[1] + '-' + dateParts[0];
-  }
-
-  selectNavigationOption(actual: number): void {
-    this.selectedNavigation = actual;
-    this.selectedCustomOptionNavigation = 0;
-  }
-
-  selectCustomNavigationOption(actual: number): void {
-    this.selectedCustomOptionNavigation = actual + 1;
-  }
-
-  createPet(petData) {
-    petData.birth = this.parseDate(petData.birth);
-    petData.user = this.user;
-    this.petApiService.createPetOld(this.user.username, petData.name, petData).subscribe(() =>
-      this.petApiService.getAllPetsOld(this.user.username).subscribe(pets => {
-        this.user.pets = pets;
-        this.selectNavigationOption(0);
-        this.registerPetForm.reset();
-      })
-    );
-  }
-
-  parseDate(birth: any): string {
-    let dateParts = birth.toString().split(' ');
-    let day = dateParts[2];
-    let month = AppComponent.getMonth(dateParts[1]);
-    let year = dateParts[3];
-
-    return year + '-' + month + '-' + day + 'T00:00:00';
-  }
-
-  getPets() {
-    console.log('Getting pets from user ' + (this.user === undefined ? 'undefined' : this.user.username));
-  }
+  title = 'My Pet Care';
+  user: User;
+  isLogin = true;
+  selectedNavigation = 0;
+  selectedCustomOptionNavigation = 0;
+  arePetsObtained = false;
+  registerPetForm;
+  img = '';
+  navigationOptions = [
+    'My Pets', 'Pet\'s Community', 'My Walks', 'Near Establishments', 'Calendar', 'Achievements', 'Settings'
+  ];
+  navigationCustomOptions = [
+    ['Register new pet'], [], [], [], [], [], []
+  ];
+  registerPetControl = new FormControl('', [
+    Validators.required
+  ]);
 
   private static getMonth(month: string): string {
     switch (month) {
@@ -172,5 +75,107 @@ export class AppComponent implements OnInit {
     }
 
     return '12';
+  }
+
+  doGoogleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('profile');
+    provider.addScope('email');
+    this.afAuth.signInWithPopup(provider).then(res => {
+      this.tokenManagerService.token = res.user.refreshToken;
+      this.userApiService.getUserOld(res.user.uid).subscribe(user => {
+        this.user = user;
+        this.petApiService.getAllPetsOld(res.user.displayName).subscribe(pets => {
+          this.user.pets = pets;
+          this.isLogin = false;
+          this.arePetsObtained = true;
+          for (const pet of pets ) {
+            this.petApiService.getPetImage(res.user.displayName, pet.name).subscribe(img => {
+              pet.image = img;
+            });
+          }
+        });
+      });
+    });
+  }
+
+  doLogOut() {
+    this.afAuth.signOut().then(() => {
+      this.user = null;
+      this.isLogin = true;
+      this.arePetsObtained = false;
+    });
+  }
+
+  info(pet: Pet) {
+    pet.owner = this.user.username;
+    const dialog = this.dialog.open(DialogPetComponent, {
+      data: pet
+    });
+    dialog.afterClosed().subscribe(() => {
+      this.petApiService.getAllPetsOld(this.user.username).subscribe(pets => {
+        this.user.pets = pets;
+        this.isLogin = false;
+        this.arePetsObtained = true;
+        for (const peta of pets ) {
+            this.petApiService.getPetImage(this.user.username, peta.name).subscribe(img => {
+              peta.image = img;
+            });
+        }
+      });
+    });
+  }
+
+  ngOnInit(): void {
+    this.registerPetForm = this.formBuilder.group({
+      name: '',
+      gender: '',
+      breed: '',
+      birth: '',
+      pathologies: ''
+    });
+
+  }
+
+  getDate(dateTime: string): string {
+    const index = dateTime.indexOf('T');
+    const date = dateTime.substring(0, index);
+    const dateParts = date.split('-');
+
+    return dateParts[2] + '-' + dateParts[1] + '-' + dateParts[0];
+  }
+
+  selectNavigationOption(actual: number): void {
+    this.selectedNavigation = actual;
+    this.selectedCustomOptionNavigation = 0;
+  }
+
+  selectCustomNavigationOption(actual: number): void {
+    this.selectedCustomOptionNavigation = actual + 1;
+  }
+
+  createPet(petData) {
+    petData.birth = this.parseDate(petData.birth);
+    petData.user = this.user;
+    this.petApiService.createPetOld(this.user.username, petData.name, petData).subscribe(() =>
+      this.petApiService.getAllPetsOld(this.user.username).subscribe(pets => {
+        this.user.pets = pets;
+        this.selectNavigationOption(0);
+        this.registerPetForm.reset();
+      })
+    );
+  }
+
+  parseDate(birth: any): string {
+    const dateParts = birth.toString().split(' ');
+    const day = dateParts[2];
+    const month = AppComponent.getMonth(dateParts[1]);
+    const year = dateParts[3];
+
+    return year + '-' + month + '-' + day + 'T00:00:00';
+  }
+
+  getPets() {
+    console.log('Getting pets from user ' + (this.user === undefined ? 'undefined' : this.user.username));
   }
 }
